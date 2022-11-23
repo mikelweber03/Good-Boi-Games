6f79f726d243348c647f78a20de79d21e932827b
Luciano-Gennaro <Luciano-Gennaro.Vogt@gmx.de>
2022-11-23 12:16:29 +0100

    Dash and HDRP Pipeline

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float gravityModifier;
-    public float movementSpeed = 10f;
-    public float jumpHigh = 10f;
-    public float horizontalInput;
+    public bool gameOver = false;
+    public bool isOnGround = true;
     private Rigidbody playerRb;
+    public float movementSpeed = 10f;
+    public float jumpForce = 20f;
+    public float horizontalInput;
+    public float gravityModifier;
+    
+    [Header("Dashing")]
+    public bool canDash = true;
+    public float timeBtweDashes;
+    public float dashJumpIncrease;
+    public float dashSpeed;
+    public float dashingTime;
+    public float startDashTime;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        dashingTime = startDashTime;
     }
 
     // Update is called once per frame
@@ -21,13 +34,52 @@ public class PlayerMovement : MonoBehaviour
     {
         // eingabe für die bewegung in wertikalerweise 
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * movementSpeed);
-
-        // input für den sprung
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!gameOver)
         {
-            playerRb.AddForce(Vector3.up * jumpHigh, ForceMode.Impulse);
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+       
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround )
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+        
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player is on hart surves and gives him the ability to jump again
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        
+    }
+    void DashAbility()
+    {
+        if (canDash)
+        {
+            StartCoroutine(Dash());
         }
     }
+    IEnumerator Dash()
+    {
+        canDash = false;
+        movementSpeed = dashSpeed;
+        jumpForce = dashJumpIncrease;
+        yield return new WaitForSeconds(dashingTime);
+        movementSpeed = 10f;
+        jumpForce = 20f;
+        yield return new WaitForSeconds(timeBtweDashes);
+        canDash = true;
+
+        }
+
 }
