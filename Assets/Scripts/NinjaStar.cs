98c30208246cae0a392dbcbb2ba8c977aaf0920d
Luciano-Gennaro <Luciano-Gennaro.Vogt@gmx.de>
2022-11-24 18:47:44 +0100

    main menu font

--- Assets/Scripts/NinjaStar.cs
+++ Assets/Scripts/NinjaStar.cs
@@ -14,7 +14,7 @@ public class NinjaStar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
