3c3125fdcab28001ef9c0df0ab594768c8077f5e
Kenneth Driesch <kdriesch@yahoo.com>
2022-12-09 18:43:58 +0100

    Added Background + Parallax Effect

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+    [SerializeField] private bool infiniteHorizontal;
+
+    private Transform cameraTransform;
+    
+    private Vector3 lastCameraPosition;
+
+    private float textureUnitSizeX;
+
+    
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+
+        if (infiniteHorizontal)
+        {
+            if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+            {
+                float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+                transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+            }
+        }
+    }
+
+}
